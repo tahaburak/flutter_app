99bf907c6039f024feba35dcf69a639d161d1c8a
Taha Burak <tahaburak.koc@gmail.com>
2020-02-03 06:33:52 +0300

    integrated FCM

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+
+FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 
 void main() => runApp(MyApp());
 
@@ -44,6 +47,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    setUpNotification();
+  }
+
+  void setUpNotification() async {
+    _firebaseMessaging.getToken().then((token) {
+      print(token);
+    });
+
+    _firebaseMessaging.configure(
+        onMessage: (Map<String, dynamic> message) async {
+          print("Message: $message");
+        }, onResume: (Map<String, dynamic> message) async {
+      print("Message: $message");
+    }, onLaunch: (Map<String, dynamic> message) async {
+      print("Message: $message");
+    });
+  }
+
   int _counter = 0;
 
   void _incrementCounter() {
